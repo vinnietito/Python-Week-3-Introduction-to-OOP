d8ad62a2a87bd4dc6ad1b21f4762bc864446e327
vinnietito <vincentnjuguna69@gmail.com>
2024-11-27 19:13:48 +0300

    Commits

--- car.py
+++ car.py
@@ -1,2 +1,11 @@
 class Car:
-    
+    #Attributes (Properties)
+    def __init__(self, make, model, color):
+        self.make = make
+        self.model = model
+        self.color = color
+        
+        # Method
+        def start(self):
+            return f"The "
+        pass
